d2dc5e27f7b1b588b2ff99c4c942dd4a8b141c5f
SeductiveWalrus <n.j.tremblay14@gmail.com>
2018-05-20 21:08:36 -0400

    Added copy button

--- public/main.js
+++ public/main.js
@@ -1,6 +1,7 @@
 let urlForm = document.getElementById("urlForm");
 let urlInput = document.getElementById("url");
 let urlResult = document.getElementById("urlResult");
+let copyButton = document.getElementById("copyButton");
 
 urlForm.onsubmit = e =>{
     e.preventDefault();
@@ -8,6 +9,7 @@ urlForm.onsubmit = e =>{
     let request = new XMLHttpRequest();
         request.onreadystatechange = () =>{
         if(request.readyState == 4 && request.status == 200){
+            document.getElementById("resultContainer").style.display = "initial";
             urlResult.innerHTML = request.response;
         }
     };
@@ -15,3 +17,14 @@ urlForm.onsubmit = e =>{
     request.setRequestHeader("Content-Type", "text/plain");
     request.send(longURL);
 };
+
+copyButton.onclick = e =>{
+    let textarea = document.createElement("textarea");
+    textarea.id = "temp_element";
+    textarea.style.height = 0;
+    document.body.appendChild(textarea);
+    textarea.value = urlResult.innerHTML;
+    textarea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textarea);
+}
